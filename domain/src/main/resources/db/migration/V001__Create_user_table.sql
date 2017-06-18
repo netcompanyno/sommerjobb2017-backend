80410ea3a2e4b892747142ac1d4431d31dc6e6bd
Knut Esten Melandsø Nekså <knut.neksa@gmail.com>
2017-06-18 16:29:08 +0200

    Removed password hash from user table in domain-module

--- domain/src/main/resources/db/migration/V001__Create_user_table.sql
+++ domain/src/main/resources/db/migration/V001__Create_user_table.sql
@@ -1,7 +1,6 @@
  CREATE TABLE "user" (
    id SERIAL PRIMARY KEY,
    email VARCHAR(255) NOT NULL,
-   name VARCHAR(255) NOT NULL,
-   password_hash CHAR(60) NOT NULL
+   name VARCHAR(255) NOT NULL
  );
 
