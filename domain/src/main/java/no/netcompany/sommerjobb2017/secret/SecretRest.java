02327cf7ff182c76bf60eab64ec1489b248f3204
Knut Esten Melandsø Nekså <knut.neksa@gmail.com>
2017-06-19 08:13:53 +0200

    Changed name of session id, and removed context path.

--- domain/src/main/java/no/netcompany/sommerjobb2017/secret/SecretRest.java
+++ domain/src/main/java/no/netcompany/sommerjobb2017/secret/SecretRest.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Objects;
 
 @RestController
-@RequestMapping("secret")
+@RequestMapping("/api/secret")
 public class SecretRest {
     private final SecretService secretService;
 
