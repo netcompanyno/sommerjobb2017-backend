890dcd3167bc0471961e790bfca722374ef9fe8e
Knut Esten Melandsø Nekså <knut.neksa@gmail.com>
2017-06-20 20:07:49 +0200

    Moved config for connecting to oauth2 server to its own class

---
+++ domain/src/main/java/no/netcompany/sommerjobb2017/config/OAuth2ClientConfig.java
@@ -0,0 +1,91 @@
+package no.netcompany.sommerjobb2017.config;
+
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
+import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
+import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+import javax.servlet.Filter;
+import java.util.Objects;
+
+/**
+ * @author Knut Esten Melandsø Nekså
+ */
+// Uncomment to enable @Secured for authorization based on authorities (roles).
+//@EnableGlobalMethodSecurity(securedEnabled = true)
+@Configuration
+@EnableOAuth2Sso
+@Order(1)
+public class OAuth2ClientConfig extends WebSecurityConfigurerAdapter {
+    private final OAuth2ClientContext clientContext;
+    private final AuthorizationCodeResourceDetails oauth2Client;
+    private final ResourceServerTokenServices resourceServerTokenServices;
+    private final AuthenticationSuccessHandler authenticationSuccessHandler;
+
+    public OAuth2ClientConfig(final OAuth2ClientContext clientContext,
+                              final AuthorizationCodeResourceDetails oauth2Client,
+                              final ResourceServerTokenServices resourceServerTokenServices,
+                              final AuthenticationSuccessHandler authenticationSuccessHandler) {
+        Objects.requireNonNull(authenticationSuccessHandler);
+        this.authenticationSuccessHandler = authenticationSuccessHandler;
+        Objects.requireNonNull(resourceServerTokenServices);
+        this.resourceServerTokenServices = resourceServerTokenServices;
+        Objects.requireNonNull(oauth2Client);
+        this.oauth2Client = oauth2Client;
+        Objects.requireNonNull(clientContext);
+        this.clientContext = clientContext;
+    }
+
+    @Override
+    protected void configure(final HttpSecurity http) throws Exception {
+        http
+                .antMatcher("/**")
+                .authorizeRequests()
+
+                .antMatchers("/", "/login**", "/webjars/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+
+                .and()
+                .logout()
+                .logoutSuccessUrl("/").permitAll()
+
+                .and()
+                .csrf()
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+
+                .and()
+                .addFilterBefore(ssoFilter(), BasicAuthenticationFilter.class);
+    }
+
+    @Bean
+    public FilterRegistrationBean oauth2ClientFilterRegistration(OAuth2ClientContextFilter filter) {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setFilter(filter);
+        registration.setOrder(-100);
+        return registration;
+    }
+
+    private Filter ssoFilter() {
+        OAuth2ClientAuthenticationProcessingFilter ssoFilter = new OAuth2ClientAuthenticationProcessingFilter(
+                "/login");
+        OAuth2RestTemplate facebookTemplate = new OAuth2RestTemplate(oauth2Client, clientContext);
+        ssoFilter.setRestTemplate(facebookTemplate);
+        ssoFilter.setTokenServices(resourceServerTokenServices);
+        ssoFilter.setAuthenticationSuccessHandler(authenticationSuccessHandler);
+        return ssoFilter;
+    }
+}
